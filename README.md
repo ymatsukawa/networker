222fed5622c45adf6e8da4ed34f0ab30a7b1d977
Yusuke Matsukawa <ymatsukawa27@gmail.com>
2022-07-16 00:00:09 +0900

    enable to put and delete (#3)
    * added put and delete
    
    * fixed version
    
    * fixed README.md

--- README.md
+++ README.md
@@ -15,13 +15,25 @@ In `irb`
 
 ```
 require 'networker'
-
 Networker::Http.req(:file, '/path/to/request_file.json')
+
+# ex.)
+Networker::Http.req(:file, './spec/FILES/http_get.json')
+=>
+{:status=>200,
+ :headers=>
+  {"content-type"=>"text/html;charset=utf-8",
+   "content-length"=>"197",
+   .
+   .
+   },
+ :body=>
+  "<!DOCTYPE html>\n<html lang=\"en\">...</body>\n</html>\n"}
 ```
 
 example file format is in [spec/FILES](./spec/FILES)
 
-* `params` equals `query params` or `post body`
+* `params` equals `query params` or `body of post, put or delete`
 
 ## Testing
 
@@ -31,6 +43,10 @@ $ rake test:server
 # on another console
 $ rspec spec
 ```
+
+When source code is changed on server running, please restart.
+
+Hot-reload is not supported.
 
 ## License
 
--- spec/client/http_spec.rb
+++ spec/client/http_spec.rb
@@ -1,83 +1,70 @@
 require_relative "../../lib/networker/client/http"
 
 RSpec.describe Networker::Client::Http do
-  shared_examples_for "response html with status" do |status|
-    it do
-      subject
-      expect(client.status).to eq status
-      expect(client.headers).to include({ "content-type" => "text/html;charset=utf-8" })
-    end
-  end
-
-  describe "#get" do
+  describe "#get, #post, #put, #delete" do
     let(:client) { Networker::Client::Http.new(url, req_headers, params) }
-    subject { client.get }
+
+    context "when request is valid" do
+      where(:method, :url_, :params_, :expected_body_message, :expected_status) do
+        [
+          [:get,    "http://localhost:3000/example/html",    nil,                                 "This is for HTML Test", 200],
+          [:post,   "http://localhost:3000/example/machine", { nut: 3, iron_plate: 5 },           "machine created",       201],
+          [:put,    "http://localhost:3000/example/clothes", { necktie: "gray", suit: "stripe" }, "clothes updated",       200],
+          [:delete, "http://localhost:3000/example/paper",   { paper_id: 123 },                   "paper deleted",         200]
+        ]
+      end
+      with_them do
+        subject { client.send(method) }
+        let(:url) { url_ }
+        let(:req_headers) { {} }
+        let(:params) { params_ }
     
-    context "when request only url" do
-      let(:url) { "http://localhost:3000/example/html" }
-      let(:req_headers) { {} }
-      let(:params) { nil }
-
-      it_should_behave_like "response html with status", 200
-      it "should success response html" do
-        subject
-
-        expect(client.body).to include "This is for HTML Test"
-      end
-    end
-
-    context "when request with query" do
-      let(:url) { "http://localhost:3000/example/html" }
-      let(:req_headers) { {} }
-      let(:params) { { first_name: "yamada", last_name: "taro", additional: "more-query" } }
-
-      it_should_behave_like "response html with status", 200
-      it "should response with passed query" do
-        subject
-        ["yamada", "taro"].each do |expected|
-          expect(client.body).to include expected
+        it "should get success response html" do
+          subject
+          expect(client.status).to eq expected_status
+          expect(client.headers).to include({ "content-type" => "text/html;charset=utf-8" })
+          expect(client.body).to include expected_body_message
         end
-        expect(client.body).not_to include "more-query"
+      end
+
+      context "when queries are given" do
+        subject { client.get }
+        let(:url) { "http://localhost:3000/example/html" }
+        let(:req_headers) { {} }
+        let(:params) { { first_name: "yamada", last_name: "taro", additional: "more-query" } }
+  
+        it "should response with passed query" do
+          subject
+          ["yamada", "taro"].each do |expected|
+            expect(client.body).to include expected
+          end
+          expect(client.body).not_to include "more-query"
+        end
       end
     end
 
-    context "when request failed" do
-      let(:url) { "http://localhost:3000/doesnotexist" }
-      let(:req_headers) { {} }
-      let(:params) { nil }
-
-      it_should_behave_like "response html with status", 404
-      it "should response with 404" do
-        subject
-        expect(client.body).to include "404 NOT FOUND"
+    context "when request is invalid" do
+      where(:method, :url_, :params_, :expected_status) do
+        [
+          [:get,    "http://localhost:3000/blahblah",        nil,                     404],
+          [:post,   "http://localhost:3000/example/machine", { tuna: 3 },             400],
+          [:put,    "http://localhost:3000/example/clothes", { swim_suit: "stripe" }, 400],
+          [:delete, "http://localhost:3000/example/paper",   { fever_id: 123 },       400]
+        ]
+      end
+      with_them do
+        subject { client.send(method) }
+        let(:url) { url_ }
+        let(:req_headers) { {} }
+        let(:params) { params_ }
+    
+        it "should get fail response" do
+          subject
+          expect(client.status).to eq expected_status
+          expect(client.headers).to include({ "content-type" => "text/html;charset=utf-8" })
+        end
       end
     end
   end
 
-  describe "#post" do
-    let(:client) { Networker::Client::Http.new(url, req_headers, params) }
-    let(:url) { "http://localhost:3000/example/machine" }
-    let(:req_headers) { {} }
-    subject { client.post }
-
-    context "when request success" do
-      let(:params) { { nut: 3, iron_plate: 5 } }
-      
-      it_should_behave_like "response html with status", 201
-      it "should response with 201" do
-        subject
-        expect(client.body).to include "machine created"
-      end
-    end
-
-    context "when request failed because body is invalid" do
-      let(:params) { { walnut: 3 } }
-      
-      it_should_behave_like "response html with status", 400
-      it "should response with 400" do
-        subject
-        expect(client.body).to include "400 Bad Request"
-      end
-    end
-  end
 end
